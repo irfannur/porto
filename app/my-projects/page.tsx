ef13ddbb88e72b458fe5c2316e5fc24f45d3e10b
irfannur238 <irfannur238@gmail.com>
2024-04-13 05:20:13 +0700

    bg

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
   return (
     
     <div
-      style={{ backgroundImage: "url(/mountains.jpg)" }}
+      style={{ backgroundImage: "url(/bg-3.jpg)" }}
       className='w-screen h-screen flex items-center justify-center bg-center bg-cover'
     >
       <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%] flex flex-wrap'>
